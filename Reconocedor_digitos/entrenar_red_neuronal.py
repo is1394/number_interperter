1628c360cbf5f8afbaa553dd496e6716c1a15766
Jorge Ayala Saenz <jorgeayalasaenz0391@gmail.com>
2017-08-09 19:00:06 -0500

    Red neuronal con Tensorflow.Funciona solo con imágenes con fondo blanco

---
+++ Reconocedor_digitos/entrenar_red_neuronal.py
@@ -0,0 +1,103 @@
+from __future__ import division, print_function, unicode_literals
+import tensorflow as tf
+import numpy as np
+
+# Common imports
+import numpy as np
+import os
+
+#MNIST imports
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+mnist = input_data.read_data_sets("/tmp/data/")
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+
+
+n_inputs = 28*28  # MNIST
+n_hidden1 = 300
+n_hidden2 = 100
+n_outputs = 10
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int64, shape=(None), name="y")
+
+def neuron_layer(X, n_neurons, name, activation=None):
+    with tf.name_scope(name):
+        n_inputs = int(X.get_shape()[1])
+        stddev = 2 / np.sqrt(n_inputs)
+        init = tf.truncated_normal((n_inputs, n_neurons), stddev=stddev)
+        W = tf.Variable(init, name="kernel")
+        b = tf.Variable(tf.zeros([n_neurons]), name="bias")
+        Z = tf.matmul(X, W) + b
+        if activation is not None:
+            return activation(Z)
+        else:
+            return Z
+
+with tf.name_scope("dnn"):
+    hidden1 = neuron_layer(X, n_hidden1, name="hidden1",
+                           activation=tf.nn.relu)
+    hidden2 = neuron_layer(hidden1, n_hidden2, name="hidden2",
+                           activation=tf.nn.relu)
+    logits = neuron_layer(hidden2, n_outputs, name="outputs")
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
+                                                              logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+learning_rate = 0.01
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 40
+batch_size = 50
+
+print("Training started....")
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for iteration in range(mnist.train.num_examples // batch_size):
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        acc_test = accuracy.eval(feed_dict={X: mnist.test.images,
+                                            y: mnist.test.labels})
+        print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+
+    save_path = saver.save(sess, "./red_neuronal_entrenada.ckpt")
+
+print("Training finished....")
+print("Testing started...")
+
+with tf.Session() as sess:
+    saver.restore(sess, "./red_neuronal_entrenada.ckpt") # or better, use save_path
+    X_new_scaled = mnist.test.images[:20]
+    Z = logits.eval(feed_dict={X: X_new_scaled})
+    y_pred = np.argmax(Z, axis=1)
+
+print("Predicted classes:", y_pred)
+print("Actual classes:   ", mnist.test.labels[:20])
+print("Testing finished...")
+
